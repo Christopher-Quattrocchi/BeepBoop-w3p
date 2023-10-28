981245c30d9d41b7fc31cad06378ac44b4c0fdeb
Christopher Quattrocchi <chrisquattrocchi@gmail.com>
2023-10-28 09:59:21 -0700

    change output function to display an ul, fixed it so that the validation function still works correctly

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,7 @@
 //UI Logic
-window.addEventListener("load", function() {
+window.addEventListener("load", function () {
     document.querySelector("form#input-form").addEventListener("submit", handleSubmission);
     document.querySelector("form#input-form2").addEventListener("submit", bizarroOutput);
-
 })
 
 function handleSubmission(event) {//handles the UI for the first form
@@ -16,11 +15,9 @@ function handleSubmission(event) {//handles the UI for the first form
     } else {
         outputFunction(validation);
     }
-    
-
 }
 
-function bizarroOutput (event) {//confusingly named funtion that outputs the alternate string
+function bizarroOutput(event) {//confusingly named funtion that outputs the alternate string
     event.preventDefault();
     const inputNumber2 = parseInt(document.getElementById("input-text2").value);
     const inputName2 = document.getElementById("input-name2").value;
@@ -31,15 +28,25 @@ function bizarroOutput (event) {//confusingly named funtion that outputs the alt
     } else {
         outputFunction(validation);
     }
-    
 }
 
 function outputFunction(output) {//this function reduces redundancy
-    pEle = document.createElement("p");
-    divEle = document.querySelector("div#output-div");
+    const pEle = document.createElement("p");
+    const ulEle = document.createElement("ul");
+    const divEle = document.querySelector("div#output-div");
     document.querySelector("div#output-div").innerHTML = "";
-    pEle.append(output);
-    divEle.append(pEle);
+    divEle.append(ulEle);
+    for (let i = 0; i < output.length; i++) {
+        if (!Array.isArray(output)) {
+            pEle.append(output);
+            divEle.append(pEle);
+            break;
+        } else {
+            const liEle = document.createElement("li");
+            liEle.append(output[i]);
+            ulEle.append(liEle);
+        }
+    }
 }
 
 function validationCheck(inputNumber, inputName) {//Checks for valid number that won't blow up the computer and checks for name because I don't feel like changing the lower function to deal with no name
@@ -62,7 +69,7 @@ function beepBoop(inputNumber, inputName) {
     }
     const strArray = outputArray.map(String);//turn outputArray into a string array so that .includes can be used on it
     const finalArray = [];
-    for (let i= 0; i <= inputNumber; i++) {//builds altered array
+    for (let i = 0; i <= inputNumber; i++) {//builds altered array
         if (strArray[i].includes("3")) {
             finalArray.push("Won't you be my neighbor, " + inputName + "?");
         } else if (strArray[i].includes("2")) {
@@ -74,8 +81,8 @@ function beepBoop(inputNumber, inputName) {
         } else {
             finalArray.push(i);//index works in this case
         }
-    }    
-    return finalArray.join(" ");//turn array into a string, separated by space, and return it
+    }
+    return finalArray;//turn array into a string, separated by space, and return it
 }
 
 function boopBeep(inputNumber2, inputName2) {//almost the same, but starting at input and counting down to 0
@@ -85,7 +92,7 @@ function boopBeep(inputNumber2, inputName2) {//almost the same, but starting at 
     }
     const strArray = outputArray.map(String);
     const finalArray = [];
-    for (let i= 0; i <= inputNumber2; i++) {
+    for (let i = 0; i <= inputNumber2; i++) {
         if (strArray[i].includes("3")) {
             finalArray.push("Won't you be my neighbor, " + inputName2 + "?");
         } else if (strArray[i].includes("2")) {
@@ -95,6 +102,6 @@ function boopBeep(inputNumber2, inputName2) {//almost the same, but starting at 
         } else {
             finalArray.push(strArray[i]);
         }
-    }    
-    return finalArray.join(" ");//turn array into a string, separated by space
+    }
+    return finalArray;//turn array into a string, separated by space
 }
